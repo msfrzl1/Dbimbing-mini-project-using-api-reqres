d8f0ef13d73a96212432cd3677897397f8b021ec
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-16 01:17:18 +0700

    create footer components

---
+++ src/components/Fragments/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+   return (
+      <footer className='bg-gray-800 py-10 flex justify-center'>
+         <p className='text-base text-gray-400'>&copy; 2024 Your Muhammad Saifur Rizal</p>
+      </footer>
+   );
+};
+
+export default Footer;
