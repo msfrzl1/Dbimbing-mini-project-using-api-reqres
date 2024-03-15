ffff0a2feef792930bceda7074bc2544e46aac1d
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-15 22:48:33 +0700

    create handle change target value form register

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -3,8 +3,21 @@ import Card from './Card';
 import Button from '../Elements/Button';
 import Label from '../Elements/Label';
 import Input from '../Elements/Input';
+import { useState } from 'react';
 
 const FormRegister = () => {
+   const [form, setForm] = useState({
+      email: '',
+      password: '',
+   });
+
+   const onChange = (e) => {
+      setForm({
+         ...form,
+         [e.target.name]: e.target.value,
+      });
+   };
+
    return (
       <Card>
          <Card.Title>
@@ -30,7 +43,10 @@ const FormRegister = () => {
                <Input
                   id='email'
                   type='email'
+                  name='email'
                   placeholder='example@me.com'
+                  value={form.email}
+                  onChange={onChange}
                   required
                />
             </div>
@@ -42,7 +58,10 @@ const FormRegister = () => {
                <Input
                   id='password'
                   type='password'
+                  name='password'
                   placeholder='*******'
+                  value={form.password}
+                  onChange={onChange}
                   required
                />
             </div>
