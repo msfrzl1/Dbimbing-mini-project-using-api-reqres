38ccffcaf3ca44ba748e6a11f23eeb9ebf858985
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-15 22:49:41 +0700

    create page utils and create api register with reqres api

--- src/components/Utils/apiUtils.js
+++ src/components/Utils/apiUtils.js
@@ -18,3 +18,20 @@ export const login = async (payload, navigate, setLoading, setNotif) => {
       setNotif(err?.response?.data?.error);
    }
 };
+
+export const register = async (payload, navigate, setLoading, setNotif) => {
+   setLoading(true);
+   try {
+      const res = await axios.post('https://reqres.in/api/register', payload);
+      setLoading(false);
+      const token = res?.data?.token;
+      localStorage.setItem('access_token', token);
+      setNotif('Register Success');
+      setTimeout(() => {
+         navigate('/login');
+      }, 1000);
+   } catch (err) {
+      setLoading(false);
+      setNotif(err?.response?.data?.error);
+   }
+};
