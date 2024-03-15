d249da4135b0ca45d48d9084bcd7c4fbf7a2b79f
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-15 23:06:23 +0700

    create handle register with api login and logic loding, notif

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -1,20 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { register } from '../Utils/apiUtils';
 import Card from './Card';
 import Button from '../Elements/Button';
 import Label from '../Elements/Label';
 import Input from '../Elements/Input';
-import { useState } from 'react';
 
 const FormRegister = () => {
+   const [notif, setNotif] = useState('');
+   const [loading, setLoading] = useState(false);
    const [form, setForm] = useState({
       email: '',
       password: '',
    });
+   const navigate = useNavigate();
 
    const onChange = (e) => {
       setForm({
          ...form,
          [e.target.name]: e.target.value,
+      });
+   };
+
+   const handleRegister = (e) => {
+      e.preventDefault();
+      register(form, navigate, setLoading, (message) => {
+         setNotif(message);
+         setTimeout(() => {
+            setNotif('');
+         }, 2000);
       });
    };
 
@@ -34,44 +48,57 @@ const FormRegister = () => {
                </p>
             </div>
          </Card.Title>
-         <Card.Body>
-            <div className='mb-3'>
-               <Label
-                  htmlFor='email'
-                  value='Email'
+         <form onSubmit={handleRegister}>
+            <Card.Body>
+               <div className='mb-3'>
+                  <Label
+                     htmlFor='email'
+                     value='Email'
+                  />
+                  <Input
+                     id='email'
+                     type='email'
+                     name='email'
+                     placeholder='example@me.com'
+                     value={form.email}
+                     onChange={onChange}
+                     required
+                  />
+               </div>
+               <div className='mb-3'>
+                  <Label
+                     htmlFor='password'
+                     value='Password'
+                  />
+                  <Input
+                     id='password'
+                     type='password'
+                     name='password'
+                     placeholder='*******'
+                     value={form.password}
+                     onChange={onChange}
+                     required
+                  />
+               </div>
+            </Card.Body>
+            <Card.Footer>
+               <Button
+                  text={loading ? 'Loading...' : 'Register'}
+                  className='bg-cyan-600 hover:bg-cyan-700'
                />
-               <Input
-                  id='email'
-                  type='email'
-                  name='email'
-                  placeholder='example@me.com'
-                  value={form.email}
-                  onChange={onChange}
-                  required
-               />
-            </div>
-            <div className='mb-3'>
-               <Label
-                  htmlFor='password'
-                  value='Password'
-               />
-               <Input
-                  id='password'
-                  type='password'
-                  name='password'
-                  placeholder='*******'
-                  value={form.password}
-                  onChange={onChange}
-                  required
-               />
-            </div>
-         </Card.Body>
-         <Card.Footer>
-            <Button
-               text='Register'
-               className='bg-cyan-600 hover:bg-cyan-700'
-            />
-         </Card.Footer>
+               {notif && (
+                  <p
+                     className={
+                        notif === 'Register Success'
+                           ? 'text-green-500 flex items-center justify-center'
+                           : 'text-red-500 flex items-center justify-center'
+                     }
+                  >
+                     {notif}
+                  </p>
+               )}
+            </Card.Footer>
+         </form>
       </Card>
    );
 };
