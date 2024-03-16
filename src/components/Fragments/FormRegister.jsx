3d9174ebbcc47114581a774151f411b841338e02
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-16 20:54:27 +0700

    add aos animation to form register

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -34,73 +34,78 @@ const FormRegister = () => {
    };
 
    return (
-      <Card>
-         <Card.Title>
-            <div>
-               <h1>Register for new account !</h1>
-               <p>
-                  Alredy have an account?{' '}
-                  <Link
-                     className='link'
-                     to='/login'
-                  >
-                     Login
-                  </Link>
-               </p>
-            </div>
-         </Card.Title>
-         <form onSubmit={handleRegister}>
-            <Card.Body>
-               <div className='mb-3'>
-                  <Label
-                     htmlFor='email'
-                     value='Email'
-                  />
-                  <Input
-                     id='email'
-                     type='email'
-                     name='email'
-                     placeholder='example@me.com'
-                     value={form.email}
-                     onChange={onChange}
-                     required
-                  />
-               </div>
-               <div className='mb-3'>
-                  <Label
-                     htmlFor='password'
-                     value='Password'
-                  />
-                  <Input
-                     id='password'
-                     type='password'
-                     name='password'
-                     placeholder='*******'
-                     value={form.password}
-                     onChange={onChange}
-                     required
-                  />
-               </div>
-            </Card.Body>
-            <Card.Footer>
-               <Button className='bg-cyan-600 hover:bg-cyan-700'>
-                  <IconRegistered />
-                  {loading ? 'Loading...' : 'Register'}
-               </Button>
-               {notif && (
-                  <p
-                     className={
-                        notif === 'Register Success'
-                           ? 'text-green-500 flex items-center justify-center'
-                           : 'text-red-500 flex items-center justify-center'
-                     }
-                  >
-                     {notif}
+      <div
+         data-aos='flip-left'
+         data-aos-duration='1000'
+      >
+         <Card>
+            <Card.Title>
+               <div>
+                  <h1>Register for new account !</h1>
+                  <p>
+                     Alredy have an account?{' '}
+                     <Link
+                        className='link'
+                        to='/login'
+                     >
+                        Login
+                     </Link>
                   </p>
-               )}
-            </Card.Footer>
-         </form>
-      </Card>
+               </div>
+            </Card.Title>
+            <form onSubmit={handleRegister}>
+               <Card.Body>
+                  <div className='mb-3'>
+                     <Label
+                        htmlFor='email'
+                        value='Email'
+                     />
+                     <Input
+                        id='email'
+                        type='email'
+                        name='email'
+                        placeholder='example@me.com'
+                        value={form.email}
+                        onChange={onChange}
+                        required
+                     />
+                  </div>
+                  <div className='mb-3'>
+                     <Label
+                        htmlFor='password'
+                        value='Password'
+                     />
+                     <Input
+                        id='password'
+                        type='password'
+                        name='password'
+                        placeholder='*******'
+                        value={form.password}
+                        onChange={onChange}
+                        required
+                     />
+                  </div>
+               </Card.Body>
+               <Card.Footer>
+                  <Button className='bg-cyan-600 hover:bg-cyan-700'>
+                     <IconRegistered />
+                     {loading ? 'Loading...' : 'Register'}
+                  </Button>
+                  {notif && (
+                     <p
+                        className={
+                           notif === 'Register Success'
+                              ? 'text-green-500 flex items-center justify-center'
+                              : 'text-red-500 flex items-center justify-center'
+                        }
+                     >
+                        {notif}
+                     </p>
+                  )}
+               </Card.Footer>
+            </form>
+         </Card>
+      </div>
    );
 };
 
