6f0b0d9c53fb83446956b3b2fee7e5026faa24d6
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-16 21:40:35 +0700

    create new file json image dummy

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,8 +3,11 @@ import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import Auth from '../components/Layouts/Auth';
 import Card from '../components/Fragments/Card';
+import imageDummy from '../../src/components/Utils/imageDumy.json';
 
 function AutoPlay() {
+   const images = imageDummy;
+
    const settings = {
       dots: true,
       infinite: true,
@@ -16,34 +19,11 @@ function AutoPlay() {
       cssEase: 'linear',
    };
 
-   const imageDummy = [
-      {
-         id: 1,
-         image: 'https://files.realpython.com/media/Using-Flask-Login-for-User-Management-with-Flask_Watermarked.4ebd83dca67a.jpg',
-         title: 'The title of the image 1',
-      },
-      {
-         id: 2,
-         image: 'https://files.realpython.com/media/Flask-by-Example-Setting-up-Postgres-SQLAlchemy-and-Alembic_Watermarked.0cbf10a02bca.jpg',
-         title: 'The title of the image 2',
-      },
-      {
-         id: 3,
-         image: 'https://files.realpython.com/media/Creating-A-Location-Based-Web-Application-with-Django-GeoDjango-and-Postgis_Watermarked.2cb08acd38d4.jpg',
-         title: 'The title of the image 3',
-      },
-      {
-         id: 4,
-         image: 'https://pbs.twimg.com/media/F48jbYrXoAAcJWy.jpg:large',
-         title: 'The title of the image 4',
-      },
-   ];
-
    return (
       <Auth>
          <div className='slider-container mx-auto max-w-7xl mb-10'>
             <Slider {...settings}>
-               {imageDummy.map((image) => (
+               {images.map((image) => (
                   <Card
                      key={image.id}
                      className='rounded-lg overflow-hidden shadow-lg'
